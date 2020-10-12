aa96af36851cea4f0f99adb2ecaaac810653177b
PeterOtto91 <peter.otto@telenet.be>
2020-10-12 16:24:49 +0200

    HelloRest

--- HelloRest/src/main/java/be/abis/exercises/controller/ApiController.java
+++ HelloRest/src/main/java/be/abis/exercises/controller/ApiController.java
@@ -3,6 +3,7 @@ package be.abis.exercises.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import be.abis.exercises.model.Person;
@@ -14,7 +15,7 @@ public class ApiController {
 	@Autowired
 	PersonService personService;
 	
-	@GetMapping("Persons/{id}")
+	@GetMapping("persons/{id}")
 	public Person findPerson(@PathVariable("id") int id) {
 		
 		return personService.getPerson(id);
