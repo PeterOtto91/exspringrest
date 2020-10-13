68b47c5460c2cad7e8b66fef8a64c4414593c30f
PeterOtto91 <peter.otto@telenet.be>
2020-10-13 10:50:39 +0200

    EXA2 URI's correction

--- ExA2/src/main/java/be/abis/exercise/controller/PersonController.java
+++ ExA2/src/main/java/be/abis/exercise/controller/PersonController.java
@@ -27,18 +27,18 @@ public class PersonController {
 		return personservice.findPerson(id);
 	}
 	
-	@PostMapping("/login")
+	@PostMapping("persons/login")
 	public Person findPersonByMailAndPwd(@RequestBody Login login){
 		Person p1 = personservice.findPerson(login.getEmail(), login.getPassword());
 		return p1;
 	}
 	
-	@PostMapping("persons/new")
+	@PostMapping("persons")
 	public void newPerson(@RequestBody Person person) throws IOException {
 		personservice.addPerson(person);
 	}
 	
-	@PutMapping("persons/newpwd/{id}")
+	@PutMapping("persons/{id}")
 	public void changePW(@PathVariable("id") int id, @RequestBody Person person) throws IOException {
 		Person personOld = personservice.findPerson(id);
 		String pwd = person.getPassword();
@@ -50,7 +50,7 @@ public class PersonController {
 		personservice.deletePerson(id);
 	}
 	
-	@GetMapping("persons/all")
+	@GetMapping("persons")
 	public ArrayList<Person> findAllPersons() {
 		return personservice.getAllPersons();
 	}
